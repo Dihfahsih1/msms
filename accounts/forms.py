b871f97800a27909d23c76afd53b487686670738
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-09-16 15:47:07 +0300

    .

--- accounts/forms.py
+++ accounts/forms.py
@@ -302,10 +302,7 @@ class AddStudentForm(forms.ModelForm):
         'email','password','health_condition','Transfer_Certificate',
         'Father_Photo','Student_Photo','Mother_Photo','Birth_Date')
         labels = {'school':'Name of the School', 'name':'Name of the student','gender':'Gender of Student', 'religion':'Religion of      Student','Class':'Current Class of Student','stream':'Student Stream','admission_no':'Student Admission Number',
-                  'admission_date':'Admission Date','Guardian':'Student Guardian','GuardianRelationshipToStudent':'Guardian Relationship To Student' }
-        def __init__(self, *args, **kwargs):
-            super(AddStudentForm, self).__init__(*args, **kwargs)
-            self.fields['admission_date'].widget.attrs['class'] = 'datepicker'   
+                  'admission_date':'Admission Date','Guardian':'Student Guardian','GuardianRelationshipToStudent':'Guardian Relationship To Student' }   
 
 class AddStudentAttendanceForm(forms.ModelForm):
     class Meta:
