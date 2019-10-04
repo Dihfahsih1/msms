8f30fe87cb7f45d8705f173da9d177f75e9ffdd7
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-10-04 15:39:01 +0300

    .

--- accounts/urls.py
+++ accounts/urls.py
@@ -235,4 +235,5 @@ url(r'^students_of_guardian_in_form_six', views.students_of_guardian_in_form_six
 url(r'^search_student', views.search_student, name="search_student"),
 url(r'^logout', views.login, name="logout"),
 path('Addfeescollection', Addfeescollection.as_view(), name='Addfeescollection'),
+url(r'^accounting/invoice/add/$', views.invoice_create, name='invoice_create'),
 ]
